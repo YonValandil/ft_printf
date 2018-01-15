f684ccc69f85b4c99b7d44711cb8c0c8848308b4
Jimmy JOURNE <jjourne@e2r2p6.42.fr>
2018-01-15 22:24:20 +0100

    delete useless files and test, parser explaination

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jjourne <jjourne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/03 23:42:49 by jjourne           #+#    #+#             */
-/*   Updated: 2017/09/05 00:19:58 by jjourne          ###   ########.fr       */
+/*   Updated: 2018/01/15 22:22:30 by jjourne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,30 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
-
 # include "libft.h"
 
-#include <stdio.h> //
 
 #define SPECIFIER “sSpdDioOuUxXcC”
+#define MODIFIER “0123456789 -+#”
 
-typedef struct		s_coords
+typedef struct		s_result
 {
-	int				x;
-	int				y;
-}					t_coords;
+	char			buf[444];
+}					t_result;
+
+typedef struct		s_printf
+{
+	t_result		result;
+}					t_printf;
+
+/**
+** parcourt la str(chaine de format) passé en arguments de printf,
+** determine si on a un: (1)caractere simple, (2)caractere d'echappement '\', (3)caractere de specfication de format '%'
+** (1): aucune modification, l'afficher sur la sortie standard
+** (2): converti en caractere simple le char suivant, et l'afficher sur la sortie standard
+** (3): 
+**/
+int		parser();
 
 
 #endif
--- main.c
+++ main.c
@@ -5,95 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jjourne <jjourne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/03 23:36:12 by jjourne           #+#    #+#             */
-/*   Updated: 2017/09/06 04:18:18 by jjourne          ###   ########.fr       */
+/*   Created: 2018/01/15 01:43:35 by jjourne           #+#    #+#             */
+/*   Updated: 2018/01/15 22:22:32 by jjourne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	test_variatique(char *panda, ...)
-{
-    va_list     vl;
-    int         ret;
-
-    va_start(vl, panda);
-    ret = 0;
-    printf("\npanda = %s\n", panda);
-    while (ret != -1)
-    {
-        ret = va_arg(vl, int);
-        printf("\nval = %d\n", ret);
-    }
-    va_end(vl);
-}
-
-void		*ft_memjoin(void const *s1, void const *s2, size_t n1, size_t n2)
-{
-	size_t	i;
-	size_t	j;
-	void	*ns;
-
-	if (!s1 && !s2)
-		return (0);
-	if (!s1 || !s2)
-	{
-		ns = ft_memalloc((!s1) ? n2 : n1);
-		if (ns == NULL)
-			return (NULL);
-		ns = ft_memcpy(ns, ((!s1) ? (void*)s2 : (void*)s1), ((!s1) ? n2 : n1));
-		return ((unsigned char*)ns);
-	}
-	i = -1;
-	j = -1;
-	ns = (unsigned char*)ft_memalloc(n1 + n2);
-	if (ns == NULL)
-		return (NULL);
-	while (++i < n1)
-		((unsigned char*)ns)[i] = ((const unsigned char*)s1)[i];
-	while (++j < n2)
-		((unsigned char*)ns)[i++] = ((const unsigned char*)s2)[j];
-	return ((unsigned char*)ns);
-}
-
-
-
-void	ft_miniprintf(const char *pandastr, ...)
-{
-    va_list     vl;
-    int         i;
-
-    va_start(vl, pandastr);
-
-    i = 0;
-    while (pandastr[i])
-    {
-        if(pandastr[i] == '%')
-        {
-            if (pandastr[i + 1] == 's')
-                printf("%s", va_arg(vl, char*));
-            if (pandastr[i + 1] == 'd')
-                printf("%d", va_arg(vl, int));
-            if (pandastr[i + 1] == 'i')
-                printf("%d", va_arg(vl, int));
-            if (pandastr[i + 1] == 'u')
-                printf("%u", va_arg(vl, unsigned int));
-            if (pandastr[i + 1] == 'c')
-                printf("%c", (char)va_arg(vl, int));
-            i += (pandastr[i + 1] == '\0') ? 1 : 2;
-        }
-        else
-            printf("%c", pandastr[i++]);
-    }
-    va_end(vl);
-}
-
 int		main(void)
 {
-    //test_variatique("panda", 1, 2, 4, 8, 16, 32, 64, -1, 128, 255);
-    //printf("lol spiouc\n", 45);
-    ft_miniprintf("Bonjour %s ! cette belle matinée du %d %s ?\n%", "panda", 11, "septembre");
-    //ft_miniprintf("\nmy printf ->\n%s\n", argv[1]);
-
-    return (0);
+	printf(, );
+	return EXIT_SUCCESS;
 }

ab20295ec58f00ea7d2daf68495ff25556a573a7
Jimmy JOURNE <jjourne@e2r13p2.42.fr>
2018-01-16 23:34:02 +0100

    parser almost done

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jjourne <jjourne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/03 23:42:49 by jjourne           #+#    #+#             */
-/*   Updated: 2018/01/15 22:22:30 by jjourne          ###   ########.fr       */
+/*   Updated: 2018/01/16 23:21:30 by jjourne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@
 
 
 #define SPECIFIER “sSpdDioOuUxXcC”
-#define MODIFIER “0123456789 -+#”
+#define FLAG “0123456789 -+#”
+#define MODIFIER "hlLjz" //manque: q | t
 
 typedef struct		s_result
 {
@@ -30,6 +31,7 @@ typedef struct		s_result
 typedef struct		s_printf
 {
 	t_result		result;
+	int				total;
 }					t_printf;
 
 /**
@@ -37,9 +39,13 @@ typedef struct		s_printf
 ** determine si on a un: (1)caractere simple, (2)caractere d'echappement '\', (3)caractere de specfication de format '%'
 ** (1): aucune modification, l'afficher sur la sortie standard
 ** (2): converti en caractere simple le char suivant, et l'afficher sur la sortie standard
-** (3): 
+** (3): traitement de l'argument a envoyer en fonction des attributs: flags, field with, precision, format(modifieur/specifieur)
+**
+** ARGUMENT EN ENTRE: la structure printf, la va_list
+**
+** TYPE DE RETOUR:
 **/
-int		parser();
+int		parser(t_printf *print, va_list vl);
 
 
 #endif
--- main.c
+++ main.c
@@ -6,14 +6,60 @@
 /*   By: jjourne <jjourne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/15 01:43:35 by jjourne           #+#    #+#             */
-/*   Updated: 2018/01/15 22:22:32 by jjourne          ###   ########.fr       */
+/*   Updated: 2018/01/16 23:25:26 by jjourne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int		parser(t_printf *print, va_list vl)
+{
+	int i;
+	int	ret;
+	int state;
+	const char* format;
+
+	i = -1;
+	ret = 0; //nbr d'incr a passer dans format apres les flag
+	state = 0;
+	format = va_arg(vl, const char*);
+	//print->total = ft_strlen(format);
+	while (format[++i]) //main loop
+	{
+		write(1, "1\n", 2);
+		if (format[i] == '%' && format[i + 1] == '%'){ //si il y a 2 % d'affile
+			i++;
+		}else if(format[i] == '%'){ //si %, rentrer dans un chunk de flag(state == 1)
+			//ret = get_flag();
+			i = i + ret;
+			state = 1;
+		}
+		if (state == 1){ //si on est dans un chunk de flag(state=1)
+			//call the universe
+			state = 0;
+		}else{ //si on est dans un chunk standard(state=0): caractere simple
+			write(1, "2/n", 2);
+			print->result.buf[i] = format[i]; //la copie segfault
+			write(1, "3/n", 2);
+		}
+	}
+	return (42);
+}
+
+int		ft_printf(const char *format, ...)
+{
+	va_list     vl;
+	t_printf	*print;
+
+	va_start(vl, format);
+	//init(); //initialise la structure a 0 | list chainee(?)
+	parser(print, vl);
+	va_end(vl);
+	return 42; //return la taille de la chaine(format) final
+}
+
 int		main(void)
 {
-	printf(, );
+	ft_printf("coucou !");
 	return EXIT_SUCCESS;
 }

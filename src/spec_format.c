a649cb105c200586233f8d3fa97b9e9cbf186462
Jimmy JOURNE <jjourne@e3r13p7.42.fr>
2018-03-08 15:00:16 +0100

    test

--- src/spec_format.c
+++ src/spec_format.c
@@ -9,13 +9,20 @@ void 	o(t_printf *data, va_list vl, t_type specifier)
 
 void 	x(t_printf *data, va_list vl, t_type specifier)
 {
+	int i;
+	int tmp;
 	char *a;
 
 	specifier.d = va_arg(vl, int);
 	printf("val x = %ld\n", specifier.d);
 	a = ft_lltoa_base(specifier.d, 16);
+	printf("val x2 = %ld\n", specifier.d);
 	printf("val a = %s\n", a);
+
 	// apply_modifier(data, &specifier, 0);
+	tmp = ft_strlen(a);
+	while (++i <= tmp)
+		add_to_result(data, a[i], 1);
 }
 
 void 	X(t_printf *data, va_list vl, t_type specifier)
